9508a55dfb7536d1df76d468daff0471485f09cf
Maxine <146043018+MaxineToTheStars@users.noreply.github.com>
2023-12-09 15:10:14 -0500

    Build script changes
    + Updated build script
    + Updated dependency list

--- src/install.sh
+++ src/install.sh
@@ -37,7 +37,7 @@ function _configure_environment() {
 
 function _installer_install_dependencies() {
 	# Install dependencies from Debian package repository
-	sudo apt-get install $(awk '{print $1}')
+	sudo apt-get install $(awk '{print $1}' ./resources/dependencies.txt)
 
 	# Git clone additional dependencies
 	git clone https://gitlab.freedesktop.org/emersion/libdisplay-info.git
@@ -45,27 +45,35 @@ function _installer_install_dependencies() {
 }
 
 function _installer_build_clone_packages() {
-	# Build libdisplay-info
+	# Switch to libdisplay-info directory
 	cd ./libdisplay-info
 	
-	# Setup build using meson
+	# Set-up build
 	meson setup build/
-	# Build/Compile with ninja
+	# Build
 	ninja -C build/
 
-	# Go back one directory
+	# Go up a directory
 	cd ..
 
 	# Clean up
 	rm --recursive --force ./libdisplay-info
 
-	# Build libxcb-errors
+	# Switch to libxcb-errors directory
 	cd ./libxcb-errors
 
+	# Update build info
+	autoupdate
+	# Set-up build
+	autoreconf --install --verbose --force
+	# Configure the build
+	./configure --disable-silent-rules
 	# Build
 	make
+	# Sanity check
+	make check
 
-	# Go back one directory
+	# Go up a directory
 	cd ..
 
 	# Clean up
